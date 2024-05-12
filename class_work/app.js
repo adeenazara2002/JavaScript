7c1bd8b36e9ab3f584714dfe32d9723adae5c297
adeenazara2002 <adinazara2001@gmail.com>
2024-05-12 17:40:01 +0500

    Task

---
+++ class_work/app.js
@@ -0,0 +1,14 @@
+
+let age = 13;
+let gender = "female";
+let grade = "A";
+
+if(age >= 13 && gender == "female" && grade == "A"){
+    alert("Enrolled");
+}
+
+else{
+    alert("Cannot enrolled")
+    
+}
+
