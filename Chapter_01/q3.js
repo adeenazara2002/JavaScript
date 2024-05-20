7ce0e091726e239ef21fec816727eb28869e00c7
adeenazara2002 <adinazara2001@gmail.com>
2024-05-20 12:02:36 +0500

    Chapter 02

---
+++ Chapter_01/q3.js
@@ -0,0 +1,5 @@
+alert("Welcome to JS Land...\n Happy Coding!")
+alert("Welcome to JS Land")
+alert("Happy Coding!\nPrevent this page from creting additional dialogues")
+console.log("Hello....I can run JS through my web browser's console")
+
---
+++ Chapter_02/app.js
@@ -0,0 +1,36 @@
+alert("Chapter 02")
+
+let userName = "Adina"
+let myName = "Adina Zara"
+
+// Q3
+let message = "Hello World"
+alert(message)
+
+// Q4
+
+let name = "John"
+let age = 13
+let prof = "Certified mobile and web application developer"
+alert(name)
+alert(age)
+alert(prof)
+
+// Q5
+
+let pizza = "PIZZA \n";
+pizza += " PIZZ \n PIZ \n PI \n P";
+alert(pizza);
+
+// Q6
+
+let email = "adinazara09@gmail.com"
+alert("My email address is "+email)
+
+// q7
+
+let book = "A Smarter way to learn javascript"
+alert("I am trying to learn from the book "+book)
+
+document.write("Yah! I can write HTML content through JavaScript")
+
