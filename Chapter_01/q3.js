7fd2e37077cf9427527d26cc83715ae93d745174
adeenazara2002 <adinazara2001@gmail.com>
2024-05-20 13:04:01 +0500

    Updated

--- Chapter_01/q3.js
+++ Chapter_01/q3.js
@@ -1,5 +1,12 @@
+// 4. Write a script to display following messages in sequence:
+
 alert("Welcome to JS Land...\n Happy Coding!")
+
+// 5. Generate the following message through browser’s
+// developer console:
+
 alert("Welcome to JS Land")
-alert("Happy Coding!\nPrevent this page from creting additional dialogues")
+
+alert("Happy Coding!\nPrevent this page from creating additional dialogues")
 console.log("Hello....I can run JS through my web browser's console")
 
--- Chapter_02/app.js
+++ Chapter_02/app.js
@@ -1,13 +1,24 @@
 alert("Chapter 02")
 
+// 1. Declare a variable called username.
+
 let userName = "Adina"
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
 let myName = "Adina Zara"
 
-// Q3
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
 let message = "Hello World"
 alert(message)
 
-// Q4
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
 
 let name = "John"
 let age = 13
@@ -16,18 +27,24 @@ alert(name)
 alert(age)
 alert(prof)
 
-// Q5
+// 5. Write a script to display the following alert using one JS
+// variable:
 
 let pizza = "PIZZA \n";
 pizza += " PIZZ \n PIZ \n PI \n P";
 alert(pizza);
 
-// Q6
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
 
 let email = "adinazara09@gmail.com"
 alert("My email address is "+email)
 
-// q7
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
 
 let book = "A Smarter way to learn javascript"
 alert("I am trying to learn from the book "+book)
