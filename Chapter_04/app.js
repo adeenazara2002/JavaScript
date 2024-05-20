1702fa49d36d9a7a9510c3a2e549ce44d5d2a20f
adeenazara2002 <adinazara2001@gmail.com>
2024-05-20 13:19:40 +0500

    Chapter 04

---
+++ Chapter_04/app.js
@@ -0,0 +1,8 @@
+// CHAPTER 04
+
+// Variable Names Legal and Illegal
+
+document.write("<b>Rules for naming JS variables</b><br> ")
+
+document.write("Variable names can only contain number, $ and _. For example: $my_Variable <br> Variables must begin with a letter, $ or_. For example $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords")
+
