2b696117a6ae96b1303b1e8637b65c8e2e318095
adeenazara2002 <adinazara2001@gmail.com>
2024-06-23 18:51:24 +0500

    23-06-24

--- class_unknowm/task.js
+++ class_unknowm/task.js
@@ -1,5 +1,16 @@
-let input = prompt('Enter your city name');
-let firstChar = input.charAt(0).toUpperCase();
-let otherChar = input.slice(1).toLowerCase();
-let concat = firstChar+otherChar;
-alert(concat);
+// let input = prompt('Enter your city name');
+// let firstChar = input.charAt(0).toUpperCase();
+// let otherChar = input.slice(1).toLowerCase();
+// let concat = firstChar+otherChar;
+// alert(concat);
+
+// Dice
+
+var bigDecimal = Math.random();  
+var improvedNum = (bigDecimal * 6) + 1; 
+var numberOfStars = Math.floor(improvedNum);
+alert(numberOfStars);
+
+var num = math.random();
+var newNum = Math.ceil(num * 6)
+alert(newNum);
---
+++ class_unknowm/task2.js
@@ -0,0 +1,4 @@
+let array = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110]
+let winners = Math.round(Math.random() * array.length);
+let win = array[winners]
+alert(win)
