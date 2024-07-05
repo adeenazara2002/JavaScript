659ddd781a0fe39082def1aba75a57f1eea66b8d
adeenazara2002 <adinazara2001@gmail.com>
2024-07-05 20:15:17 +0500

    Functions

---
+++ Functions_exercise/ex_6.1/q1.js
@@ -0,0 +1,12 @@
+// <!-- EXERCISE 6.1 -->
+
+// <!-- Create a function that takes two parameters, adds the parameters together,
+// and returns the result. -->
+
+function add(a,b) {
+    return a + b;
+}
+
+let result = add(3,5)
+console.log("Addition: "+result);
+
