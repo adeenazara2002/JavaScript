3d4fad663c2ad99cf5844b1cf8cea0ec6724e898
adeenazara2002 <adinazara2001@gmail.com>
2024-07-05 20:34:59 +0500

    Functions

---
+++ Functions_exercise/ex_6.1/q2.js
@@ -0,0 +1,13 @@
+// 2. Set up two different variables with two different values.
+// Use your function on the two variables, and output the result using
+// console.log.
+
+let num1 = 10
+let num2 = 20
+
+let result = add(num1 , num2)
+console.log(result);
+
+function add(a,b) {
+    return a + b;
+}
---
+++ Functions_exercise/ex_6.1/q4.js
@@ -0,0 +1,13 @@
+// Create a second call to the function using two more numbers as arguments
+// sent to the function.
+
+function sub(a , b){
+    return a - b;
+}
+
+let result = sub(10,20)
+console.log(result)
+
+
+let result2 = sub(30,20)
+console.log(result2)
